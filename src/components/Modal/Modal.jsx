64fd4def53614c3638cf1a476643be9332a9fae9
Yevhen Podoliaka <zanp85740@gmail.com>
2022-07-27 21:27:22 +0300

    baufix

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -11,7 +11,7 @@ export default function Modal ({children, onClose}){
   // }
   useEffect(() => {
       window.addEventListener('keydown', handleKeyDown);
-  },[])
+  })
   const handleKeyDown = e => {
     if (e.code === 'Escape') {
       onClose();
