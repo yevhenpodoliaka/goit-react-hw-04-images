4d559544de341883a49aa3f44c4aa1f1f2e33cac
Yevhen Podoliaka <zanp85740@gmail.com>
2022-07-28 13:45:56 +0300

    fixed close modal by esc

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,76 +1,82 @@
-import  {useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Searchbar from '../Searchbar/Searchbar';
-import {Container}from'./App.styled'
+import { Container } from './App.styled';
 import fetchImg from 'service/apiSersice';
 import ImageGallery from '../ImageGallery/ImageGallery';
 import Button from '../Button/Button';
 import Loader from '../Loader/Loader';
-import  Modal  from '../Modal/Modal';
+import Modal from '../Modal/Modal';
 import Message from '../Message/Message';
 
-
 export function App() {
-  const[query, setQuery]=useState('')
-  const[page,setPage]=useState(1)
-  const [totalHits, setTotalHits] = useState(null)
-  const[items,setItems]=useState([])
-  const[loading,setLoading]=useState(false)
-  const[largeImageURL,setlargeImageURL]=useState(null)
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(null);
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [largeImageURL, setlargeImageURL] = useState(null);
 
   useEffect(() => {
     if (query === '') {
-      return
+      return;
     }
-    fetchImg(query, page).then(data => {
-      setLoading(true);
-      setItems((prevState) => {return [...prevState,...data.hits]});
-      setTotalHits(data.totalHits);
-    }).catch(error => { console.log(error) })
-      .finally(()=>setLoading(false))
-},[page,query])
+
+    setLoading(true);
+    
+    fetchImg(query, page)
+      .then(data => {
+        setItems(prevState => {
+          return [...prevState, ...data.hits];
+        });
+        setTotalHits(data.totalHits);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => setLoading(false));
+  }, [page, query]);
 
   const onSubmit = value => {
     if (value.trim() === '') {
-      return
+      return;
     }
     setQuery(value);
     setPage(1);
-    setItems([])
+    setItems([]);
   };
   const handlerBtnLoadMore = () => {
-    setPage(page+1)
+    setPage(page + 1);
   };
   const showBtnLoadMore = () => {
     if (loading) {
-      return false
+      return false;
     }
     if (items.length === totalHits) {
-      return false
+      return false;
     }
-       if (totalHits > items.length) {
-      return true
+    if (totalHits > items.length) {
+      return true;
     }
-  }
-    const closeModal = () => {
-      setlargeImageURL(null)
-  }
-
-  const setImageURL = (url) => {
-    setlargeImageURL(url)
-  }
-
-    return (
-      <Container>
-        <Searchbar onSubmit={onSubmit} />
-        {totalHits === 0?<Message/>:<ImageGallery images={items} setImageURL={setImageURL} />}
-        {<Loader visible={loading} />}
-        {showBtnLoadMore()&& (
-          <Button onClick={handlerBtnLoadMore} />
-        )}
-        {largeImageURL && <Modal onClose={closeModal}><img src={largeImageURL} alt={query} /></Modal>}
-      </Container>
-    );
-
+  };
+  const closeModal = () => {
+    setlargeImageURL(null);
+  };
+  
+  return (
+    <Container>
+      <Searchbar onSubmit={onSubmit} />
+      {totalHits === 0 ? (
+        <Message />
+      ) : (
+        <ImageGallery images={items} setImageURL={setlargeImageURL} />
+      )}
+      {<Loader visible={loading} />}
+      {showBtnLoadMore() && <Button onClick={handlerBtnLoadMore} />}
+      {largeImageURL && (
+        <Modal onClose={closeModal}>
+          <img src={largeImageURL} alt={query} />
+        </Modal>
+      )}
+    </Container>
+  );
 }
-
-
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,40 +1,34 @@
-// import  {useEffect} from 'react';
+import { useEffect } from 'react';
 import { Overlay, ModalCard } from './Modal.styled';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 const modalEl = document.querySelector('#modal');
-export default function Modal ({children, onClose}){
-  //   useEffect(() => {
-  //   window.addEventListener('keydown', handleKeyDown);
-  
-  // })
+export default function Modal({ children, onClose }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
 
-  // useEffect(() => {
-  //   window.addEventListener('keydown', handleKeyDown);
-  //   return    window.removeEventListener('keydown', handleKeyDown);
-  // })
-
-
-  // const handleKeyDown = e => {
-  //       console.log('esc');
-  //   if (e.code === 'Escape') {
-  //     console.log('esc');
-  //     onClose();
-  //   }
-  // };
+  const handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  };
   const handleOverlayClick = e => {
     if (e.currentTarget === e.target) {
       onClose();
     }
   };
 
-    return createPortal(
-      <Overlay onClick={handleOverlayClick}>
-        <ModalCard>{children}</ModalCard>
-      </Overlay>,
-      modalEl
-    );
-  }
+  return createPortal(
+    <Overlay onClick={handleOverlayClick}>
+      <ModalCard>{children}</ModalCard>
+    </Overlay>,
+    modalEl
+  );
+}
 
 Modal.propType = {
   onClose: PropTypes.func.isRequired,
